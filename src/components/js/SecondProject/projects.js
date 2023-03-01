454435d9555f23357edf29833de687e8d0df2bd9
Sala2Code <109032171+Sala2Code@users.noreply.github.com>
2023-03-01 23:59:47 +0100

    Update projects.js

--- src/components/js/SecondProject/projects.js
+++ src/components/js/SecondProject/projects.js
@@ -1,9 +1,6 @@
 import {Canvas} from "@react-three/fiber";
-import { useState, useRef, useEffect, useMemo} from 'react';
-import { EffectComposer, SelectiveBloom, Selection, Bloom} from '@react-three/postprocessing';
-
-import { Layers } from 'three';
-
+import { useState, useRef} from 'react';
+import { EffectComposer, SelectiveBloom} from '@react-three/postprocessing';
 
 import Octopus from "components/JSXobj/Octopus";
 import AngelfishRight from "components/JSXobj/AngelfishRight";
@@ -16,10 +13,12 @@ const Projects = (props) => {
    const [clickDiv, setClickDiv] = useState("00");
    const [iDiv, setiDiv] = useState(0);
 
+   const lightRef = useRef();
+
     return (  
         <>
         <Canvas style={{ backgroundImage: 'linear-gradient(#0800e6, #020036)'}}>
-            <directionalLight position={[0,5,10]} intensity={0.5} />
+            <directionalLight position={[0,5,10]} intensity={0.5} ref={lightRef}/>
             <ambientLight intensity={0.1}/>
 
             <AngelfishRight position={[1.5,-3.5,-2]}
@@ -43,7 +42,7 @@ const Projects = (props) => {
             <Octopus position={[-10,-3.5,-7]}  rotation={[0,-3.14/7,0]} event={clickDiv}/>
             <Jellyfish position={[7, 0, -5]} scale={[0.3, 0.3, 0.3]} layers={1}/>
             <EffectComposer>
-                <SelectiveBloom luminanceThreshold={0} intensity={1.5} />
+                <SelectiveBloom luminanceThreshold={0} intensity={1.5} light={lightRef}/>
             </EffectComposer>
                 
                 
